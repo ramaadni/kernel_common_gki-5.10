74553101f11aa921fc7151ade24279635ceaff66
Sultan Alsawaf <sultan@kerneltoast.com>
2023-11-16 21:06:37 -0800

    drm: Convert CPU-invariant uninterruptible waits to TASK_IDLE
    These waits are invariant with respect to CPU frequency, so make them use
    TASK_IDLE in order to not contribute to load avg.
    
    Change-Id: I3e0deca5528754c67877071e5b19adf914ef70ca
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

--- drivers/gpu/drm/drm_atomic_helper.c
+++ drivers/gpu/drm/drm_atomic_helper.c
@@ -1566,7 +1566,7 @@ void drm_atomic_helper_wait_for_flip_done(struct drm_device *dev,
 		if (!crtc || !commit)
 			continue;
 
-		ret = wait_for_completion_timeout(&commit->flip_done, 10 * HZ);
+		ret = wait_for_common(&commit->flip_done, 10 * HZ, TASK_IDLE);
 		if (ret == 0)
 			DRM_ERROR("[CRTC:%d:%s] flip_done timed out\n",
 				  crtc->base.id, crtc->name);
@@ -2208,16 +2208,14 @@ void drm_atomic_helper_wait_for_dependencies(struct drm_atomic_state *old_state)
 		if (!commit)
 			continue;
 
-		ret = wait_for_completion_timeout(&commit->hw_done,
-						  10*HZ);
+		ret = wait_for_common(&commit->hw_done, 10 * HZ, TASK_IDLE);
 		if (ret == 0)
 			DRM_ERROR("[CRTC:%d:%s] hw_done timed out\n",
 				  crtc->base.id, crtc->name);
 
 		/* Currently no support for overwriting flips, hence
 		 * stall for previous one to execute completely. */
-		ret = wait_for_completion_timeout(&commit->flip_done,
-						  10*HZ);
+		ret = wait_for_common(&commit->flip_done, 10 * HZ, TASK_IDLE);
 		if (ret == 0)
 			DRM_ERROR("[CRTC:%d:%s] flip_done timed out\n",
 				  crtc->base.id, crtc->name);
